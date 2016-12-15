5f0ace0cce6934042a77379de441f3a277e1ced4
Ivan Petrov <iv.radiant@yandex.ru>
2016-12-16 01:24:09 +0300

    latex convert

--- app/js/common.js
+++ app/js/common.js
@@ -17,5 +17,20 @@ $(function() {
 	});
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+
+	MathJax.Hub.Config({
+	  config: ["MMLorHTML.js"],
+	  jax: ["input/TeX","input/MathML","input/AsciiMath","output/HTML-CSS","output/NativeMML", "output/PreviewHTML"],
+	  extensions: ["tex2jax.js","mml2jax.js","asciimath2jax.js","MathMenu.js","MathZoom.js", "fast-preview.js", "AssistiveMML.js", "[Contrib]/a11y/accessibility-menu.js"],
+	  TeX: {
+	    extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
+	  }
+	});
+
+
+
+	
+
 
 });
